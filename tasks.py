a2fdf3fe491eb08591f6c89bb07606647ffe1619
Samuel Catchpole-Radford <sam@samss.co.uk>
2016-08-24 02:01:37 +0100

    Inital structure and menu

--- tasks.py
+++ tasks.py
@@ -1,24 +1,35 @@
-# create the list
-# welcome message to user
 # add to list
 # delete from list
 # show list
 # help
 
 import os
+import sys
 
+# clear screen function
 def clear_screen():
 	if os.name == 'nt':
 		os.system('cls')
 	else:
 		os.system('clear')
 
-def welcome():
+# quit app function
+def quit():
 	clear_screen()
-	print("***** Tasks *****\n")
-	
+	print("\n***** Tasks *****\n\nThank you for using Tasks, goodbye!\n\n")
+	sys.exit()
+
+# main menu
+def menu():
+	clear_screen()
+	print("\n***** Tasks *****\n")
+	print("Type 'new list' to create a new task list\nType 'quit' to exit (WARNING you will lose all entered data if you exit)\n")
+
+	command = input("> ")
+	if command == 'quit':
+		quit()
+
+# create the list
 
 
-tasks = []
-
-welcome()
+menu()
