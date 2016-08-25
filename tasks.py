aad9d90a064f6733d953155be7d4d6224c80ec4e
Samuel Catchpole-Radford <sam@samss.co.uk>
2016-08-26 00:04:09 +0100

    Added show_list function, ability to enter list name

--- tasks.py
+++ tasks.py
@@ -23,13 +23,44 @@ def quit():
 def menu():
 	clear_screen()
 	print("\n***** Tasks *****\n")
-	print("Type 'new list' to create a new task list\nType 'quit' to exit (WARNING you will lose all entered data if you exit)\n")
+	print("Type 'n' to create a new task list\nType 'q' to exit\n\n    ** WARNING: you will lose all entered data if you exit\n")
 
-	command = input("> ")
-	if command == 'quit':
+	command = input("> ").lower()
+	if command == 'q':
+		quit()
+	if command == 'n':
+		print("\n***** New Task List *****\n")
+		list_name = input("Please enter a title for your new list\n\n>")
+		new_task_list(list_name)
+
+# Show current list
+def show_current_list(_list, list_name):
+	num = 0
+	print("\n\n--- Currently in {} ---\n\n".format(list_name))
+	for i in _list:
+		num += 1
+		print("	{}: ".format(num) + i)
+	command = input("\n\nType 'c' to continue adding or 'quit' to exit\n\n>")
+	if command == 'c':
+		return True
+	elif command == 'quit':
 		quit()
 
-# create the list
 
+# create the list / enter items
+def new_task_list(list_name):
+	_list = []
+	clear_screen()
+	print("\n***** {} *****\n\n".format(list_name))
+	print("--- Enter item below ---\n\n")
+	while True:
+		task = input("> ")
+		if task == 'show':
+			show_current_list(_list, list_name)
+		elif task == 'quit':
+			quit()
+		_list.append(task)
+		if 'show' in _list:
+			_list.remove('show')
 
 menu()
